339308ce607df01a7ef8429c5bd845a1f9a52e33
2raymoori <2raymoori@gmail.com>
2025-09-05 18:49:39 +0200

    Setting up appwrite integration

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appWriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.njies.njiesfooddelivery",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: "68bb0bae00179cb9cd21",
+    userCollectionId:"user"
+}
